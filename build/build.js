f3e23b2c0643f53099697e00abd8cdf76e1a0b98
Jason Desrosiers <jdesrosi@gmail.com>
2023-10-03 15:12:32 -0700

    Update the build process to support more files

--- build/build.js
+++ build/build.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import { readFileSync, writeFileSync } from "node:fs";
+import { dirname, basename } from "node:path";
+import { argv } from "node:process";
 import { reporter } from "vfile-reporter";
 import { remark } from "remark";
 import remarkCodeTitles from "./remark-code-titles.js";
@@ -18,8 +20,8 @@ import rehypeStringify from "rehype-stringify";
 
 dotenv.config();
 
-(async function () {
-  const md = readFileSync(0, "utf-8");
+const build = async (filename) => {
+  const md = readFileSync(filename, "utf-8");
   const html = await remark()
     .use(remarkPresetLintMarkdownStyleGuide)
     .use(remarkGfm)
@@ -41,7 +43,8 @@ dotenv.config();
     .use(rehypeStringify)
     .process(md);
 
-  writeFileSync(1, `<!DOCTYPE html>
+  const outfile = `${dirname(filename)}/${basename(filename, ".md")}.html`;
+  writeFileSync(outfile, `<!DOCTYPE html>
 <html>
   <head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
@@ -149,4 +152,17 @@ dotenv.config();
 </html>`);
 
   console.error(reporter(html));
+};
+
+(async function () {
+  const files = argv.slice(2);
+  if (files.length === 0) {
+    console.error("WARNING: No files built. Usage: 'npm run build -- filename.md'");
+  }
+
+  for (const filename of files) {
+    console.log(`Building: ${filename} ...`);
+    await build(filename);
+    console.log("");
+  }
 }());
